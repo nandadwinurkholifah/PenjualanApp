9d64a940f0b78262d8894a967dd84d94498faa47
nanda dwi nurkholifah <nanda.dwi2911@gmail.com>
2023-11-13 14:56:27 +0700

    commit_report

--- app/Http/Controllers/ReportPenjualanController.php
+++ app/Http/Controllers/ReportPenjualanController.php
@@ -21,14 +21,14 @@ class ReportPenjualanController extends Controller
     public function cetak_pdf()
     {
         $penjualan = TransaksiDetail::orderBy('created_at','DESC')->get();
-        $tgl_cetak =  Carbon::now()->format('d - f - y');;
+        $tgl_cetak =  Carbon::now()->format('d-F-Y');
         $pdf = PDF::loadView('report_penjualan.report_pdf',compact('penjualan','tgl_cetak'));
         return $pdf->stream("Report Penjualan"."-".$tgl_cetak.".pdf");
     }
 
     public function cetak_excel()
     {
-        $tgl = now();
+        $tgl = Carbon::now()->format('d-m-y');
         return Excel::download(new TransaksiDetailExport, 'transaksi_detail_'.$tgl.'.xlsx');
     }
 }
--- resources/views/report_penjualan/report_pdf.blade.php
+++ resources/views/report_penjualan/report_pdf.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Report Penjualan</title>
+    <title>Document</title>
     <link rel="stylesheet" href="{{ asset('pdf.css') }}" type="text/css"> 
 </head>
 <body>
@@ -13,13 +13,8 @@
         <table class="w-full">
             <tr>
                 <td class="w-half">
-                    {{-- <div><h4>To:</h4></div>
-                    <div>John Doe</div>
-                    <div>123 Acme Str.</div> --}}
-                </td>
-                <td class="w" >
-                 <h5 style="margin-left: 16rem;">Tanggal Cetak : </h5>
-                 <h5 style="margin-left: 14rem;">{{$tgl_cetak}} </h5>
+                    <h5 style="margin-left: 38rem;">Tanggal Cetak : </h5>
+                    <h5 style="margin-left: 37rem;">{{$tgl_cetak}} </h5>
                 </td>
             </tr>
         </table>
@@ -35,76 +30,17 @@
                 <th>Tanggal</th>
                 <th>Agen</th>
             </tr>
-            <tr class="items">
-                @foreach($penjualan as $item)
-                    <td>
-                        {{$loop->iteration}}
-                    </td>
-                    <td>
-                        {{ $item->produk->nama_produk}}
-                    </td>
-                    <td>
-                        {{ $item->jumlah}}
-                    </td>
-                    <td>
-                        @rupiah($item->harga)
-                    </td>
-                    <td>
-                        {{ $item->transaksi->tgl_penjualan}}
-                    </td>
-                    <td>
-                        {{ $item->transaksi->agen->nama_toko}}
-                    </td>
-                @endforeach
-            </tr>
+            @foreach($penjualan as $item)
+                <tr class="items">
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $item->produk->nama_produk }}</td>
+                    <td>{{ $item->jumlah }}</td>
+                    <td>@rupiah($item->harga)</td>
+                    <td>{{ $item->transaksi->tgl_penjualan }}</td>
+                    <td>{{ $item->transaksi->agen->nama_toko }}</td>
+                </tr>
+            @endforeach
         </table>
     </div>
- 
-    {{-- <div class="total">
-        Total: $129.00 USD
-    </div>
- 
-    <div class="footer margin-top">
-        <div>Thank you</div>
-        <div>&copy; Laravel Daily</div>
-    </div> --}}
-    {{-- <table class="table table-striped projects mt-2">
-        <thead>
-            <tr>
-                <th class="text-center">No</th>
-                <th class="text-center">Nama Produk</th>
-                <th class="text-center">Jumlah</th>
-                <th class="text-center">Harga</th>
-                <th class="text-center">Tanggal</th>
-                <th class="text-center">Agen</th>
-            </tr>
-        </thead>
-        <tbody>
-          @foreach ($penjualan as $data )
-           <tr>
-            <td>
-              <div class="align-middle text-center text-md">
-                <h6 class="mb-0 text-md">{{$loop->iteration}}</h6>
-              </div>
-            </td>
-            <td class="align-middle text-center text-md">
-              <h6 class="mb-0 text-md">{{ $data->produk->nama_produk}} </h6>
-            </td>
-            <td class="align-middle text-center text-md">
-              <h6 class="mb-0 text-md">{{ $data->jumlah}} </h6>
-            </td>
-            <td class="align-middle text-center text-md">
-              <h6 class="mb-0 text-md">@rupiah($data->harga)</h6>
-            </td>
-            <td class="align-middle text-center text-md">
-                <h6 class="mb-0 text-md">{{ $data->transaksi->tgl_penjualan}} </h6>
-            </td>
-            <td class="align-middle text-center text-md">
-                <h6 class="mb-0 text-md">{{ $data->transaksi->agen->nama_toko}} </h6>
-            </td>
-           </tr>
-          @endforeach 
-        </tbody>
-    </table> --}}
 </body>
 </html>
